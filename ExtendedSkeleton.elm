017e33cda0211734856a5206ff7802a69d750f17
Dzianis Dashkevich <dskecse@gmail.com>
2017-03-18 16:45:49 +0300

    Add an extended Elm Architecture skeleton

---
+++ ExtendedSkeleton.elm
@@ -0,0 +1,41 @@
+import Html exposing (..)
+
+
+-- MODEL
+
+type alias Model =
+  {}
+
+
+-- UPDATE
+
+type Msg
+  = Reset
+  | ...
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Reset ->
+      ...
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  ...
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  ...
+
+
+-- INIT
+
+init : (Model, Cmd Msg)
+init =
+  ...
